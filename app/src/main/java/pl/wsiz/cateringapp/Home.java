578ce6ce6d12ab3737515f4c3c634be0dc611e94
Ada Skrzypiec <adriannaskrzypiec@o2.pl>
2020-06-10 23:34:52 +0200

    dodanie menu kategorii, dodanie przykładowych zdjęć kategorii

--- app/src/main/java/pl/wsiz/cateringapp/Home.java
+++ app/src/main/java/pl/wsiz/cateringapp/Home.java
@@ -1,42 +1,32 @@
 package pl.wsiz.cateringapp;
 
-import android.content.Intent;
+
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
-import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.android.material.navigation.NavigationView;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
-import pl.wsiz.cateringapp.Interface.ItemClickListener;
-import pl.wsiz.cateringapp.ViewHolder.MenuViewHolder;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
-
-
-
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
-
-
-import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-
 import pl.wsiz.cateringapp.Common.Common;
-
 import pl.wsiz.cateringapp.Model.Kategorie;
+import pl.wsiz.cateringapp.Interface.ItemClickListener;
+import pl.wsiz.cateringapp.ViewHolder.MenuViewHolder;
+
 
 
 public class Home extends AppCompatActivity
@@ -135,6 +125,11 @@ public class Home extends AppCompatActivity
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.home, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
