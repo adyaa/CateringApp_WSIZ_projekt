39c5f7c4631b0f74b526c7b932ff3ef19a0d24bb
Ada Skrzypiec <adriannaskrzypiec@o2.pl>
2020-06-12 00:05:21 +0200

    edycja klasy FoodViewHolder

--- app/src/main/java/pl/wsiz/cateringapp/ViewHolder/FoodViewHolder.java
+++ app/src/main/java/pl/wsiz/cateringapp/ViewHolder/FoodViewHolder.java
@@ -1,4 +1,37 @@
 package pl.wsiz.cateringapp.ViewHolder;
 
-public class FoodViewHolder {
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.recyclerview.widget.RecyclerView;
+import pl.wsiz.cateringapp.Interface.ItemClickListener;
+import pl.wsiz.cateringapp.R;
+
+
+public class FoodViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+    public TextView food_name;
+    public ImageView food_image;
+
+    private ItemClickListener itemClickListener;
+
+
+
+    public void setItemClickListener(ItemClickListener itemClickListener) {
+        this.itemClickListener = itemClickListener;
+    }
+
+    public FoodViewHolder(View itemView) {
+        super(itemView);
+
+        food_name = itemView.findViewById(R.id.food_name);
+        food_image = itemView.findViewById(R.id.food_image);
+
+        itemView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        itemClickListener.onClick(v,getAdapterPosition(),false);
+    }
 }
