a91ece7b7225e9833963760c1b710aaef6cb5804
Ada Skrzypiec <adriannaskrzypiec@o2.pl>
2020-06-12 00:42:47 +0200

    dodanie opcji pobierania zdjęć produktów z firebase

--- app/src/main/java/pl/wsiz/cateringapp/FoodList.java
+++ app/src/main/java/pl/wsiz/cateringapp/FoodList.java
@@ -4,10 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
+import pl.wsiz.cateringapp.Common.Common;
+import pl.wsiz.cateringapp.Model.Jedzenie;
+import pl.wsiz.cateringapp.ViewHolder.FoodViewHolder;
 
 public class FoodList extends AppCompatActivity {
 
@@ -16,6 +25,10 @@ public class FoodList extends AppCompatActivity {
 
     FirebaseDatabase database;
     DatabaseReference foodList;
+
+    String categoryId = "";
+
+    FirebaseRecyclerAdapter<Jedzenie, FoodViewHolder> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +44,32 @@ public class FoodList extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
+
+
+        //Intent
+        if (getIntent() != null)
+            categoryId = getIntent().getStringExtra("CategoryId");
+        if (!categoryId.isEmpty() && categoryId != null)
+        {
+            loadListFood(categoryId);
+        }
+
+
     }
 
+    private void loadListFood(String categoryId) {
+        adapter = new FirebaseRecyclerAdapter<Jedzenie, FoodViewHolder>(Jedzenie.class, R.layout.food_item, FoodViewHolder.class,
+                foodList.orderByChild("menuId").equalTo(categoryId)
+                // działa to podobnie jak w SQLu: Select * From Jedzenie where MenuId =
+        ) {
+            @Override
+            protected void populateViewHolder(FoodViewHolder viewHolder, Jedzenie model, int pozycja) {
+                viewHolder.food_name.setText(model.getName());
+                Picasso.with(getBaseContext()).load(model.getImage()).into(viewHolder.food_image);
 
+            }
+
+        };
+
+    }
 }
